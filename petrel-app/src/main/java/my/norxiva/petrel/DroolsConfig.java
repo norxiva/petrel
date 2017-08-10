409747e128109aca063f115d1267c86c4ab3e040
norxiva <norxiva@gmail.com>
2017-08-10 18:42:14 +0800

    add drools feature

---
+++ petrel-app/src/main/java/my/norxiva/petrel/DroolsConfig.java
@@ -0,0 +1,23 @@
+package my.norxiva.petrel;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DroolsConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(KieServices.class)
+    public KieServices kieServices(){
+        return KieServices.Factory.get();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(KieContainer.class)
+    public KieContainer kieContainer(KieServices kieServices){
+        return kieServices.getKieClasspathContainer();
+    }
+}
